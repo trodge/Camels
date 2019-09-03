62fa5615651e0224da0c5e9f5268098bd8a4ee4e
U-DESKTOP-HI8IHO4\Thomas <thomasrodgers271@gmail.com>
2019-09-03 10:56:11 -0700

    tweaked

--- optimize.py
+++ optimize.py
@@ -1,6 +1,6 @@
 import sympy as sp
 Pa, Qa, Ra, Pb, Qb, Rb, Pc, Qc, Rc, P = sp.symbols('Pa, Qa, Ra, Pb, Qb, Rb, Pc, Qc, Rc, P', real = True, positive = True)
-f = Qa / Ra - Qb / Rb
-g = Qb / Rb - Qc / Rc
-h = Pa * Qa + Pb * Qb + Pc * Qc - P 
+f = Pa * Qa + Pb * Qb + Pc * Qc - P 
+g = Qa / Ra - Qb / Rb
+h = Qb / Rb - Qc / Rc
 print(sp.solvers.solve((f, g, h), (Qa, Qb, Qc)))
