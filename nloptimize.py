fc45a312f9b26034a2402cb4703bcfdbc3fef30b
U-DESKTOP-HI8IHO4\Thomas <thomasrodgers271@gmail.com>
2019-09-03 11:22:13 -0700

    nonlinear solution

---
+++ nloptimize.py
@@ -0,0 +1,6 @@
+import sympy as sp
+Aa, Ia, Pa, Ra, Sa, Ab, Ib, Pb, Rb, Sb, P = sp.symbols('Aa, Ia, Pa, Ra, Sa, Ab, Ib, Pb, Rb, Sb, P', real = True, positive = True)
+f = (Aa - (Ia - sp.sqrt((Ia - Sa * Aa) ** 2 + Sa * Pa * 2)) / Sa) / Ra - (Ab - (Ib - sp.sqrt((Ib - Sb * Ab) ** 2 + Sb * Pb * 2)) / Sb) / Rb
+g = Pa + Pb - P
+for s in sp.solve((f, g), (Pa, Pb)):
+    print(sp.factor(s), '\n')
