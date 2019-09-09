f5913ca9611d84616ea1efbac3842cd0bde735e6
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-09-09 03:44:58 -0700

    started neural nets

---
+++ net.cpp
@@ -0,0 +1,25 @@
+/*
+ * This file is part of Camels.
+ *
+ * Camels is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Camels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Camels.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * © Tom Rodgers notaraptor@gmail.com 2017-2019
+ */
+
+Net::Net(const std::vector<unsigned int> &sz) {
+    layers.reserve(sz.size());
+    for (auto s : sz) {
+        layers.push_back(std::vector<Node>(s));
+    }
+}
---
+++ net.hpp
@@ -0,0 +1,33 @@
+/*
+ * This file is part of Camels.
+ *
+ * Camels is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Camels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Camels.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * © Tom Rodgers notaraptor@gmail.com 2017-2019
+ */
+
+#ifndef NET_H
+#define NET_H
+
+struct Node {
+    double value = 0;
+}
+
+class Net {
+    std::vector<std::vector<Node>> layers;
+public:
+    Net(const std::vector<unsigned int> &sz);
+}
+
+#endif // NET_H
